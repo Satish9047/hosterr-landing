3fa0d1e1e6c78d3579b1bda30e771817010dc2d8
Satish9047 <satishprajapati930@gmail.com>
2024-02-16 13:31:37 +0545

    add responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,22 +5,26 @@ const Navbar: React.FC = () => {
     return (
         <div className='navbar'>
             <div className='navbar__div'>
-                <figure>
-                    <img src='/src/assets/Logo.svg' alt='logo' />
+                <figure className='navbar__div__fig'>
+                    <img className='img-logo' src='/src/assets/Logo.svg' alt='logo' />
                 </figure>
-                <span>Hosterr is Hiring</span>
-
+                <h2>Hosterr is Hiring</h2>
             </div>
+
             <ul className='navbar__ul'>
                 <li>Plan</li>
                 <li>Find Domain</li>
                 <li>Why Hosterr</li>
             </ul>
-            <div>
+
+            <div className='navbar__div-btn'>
                 <button className='navbar__div-btn__sign-up'>Sign</button>
                 <button className='navbar__div-btn__wait-list'>Join Waitlist</button>
-
             </div>
+
+            <figure className='navbar__menu'>
+                <img className='img-menu' src='/src/assets/menu.svg' alt='menu' />
+            </figure>
         </div>
     )
 }
