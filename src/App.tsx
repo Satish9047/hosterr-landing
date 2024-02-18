3f8c5d833bd93de06aafa68117f0685f30827195
Satish9047 <satishprajapati930@gmail.com>
2024-02-18 13:14:21 +0545

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Navbar />
       <Header />
+      <Footer />
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
         <div className='header'>
             <div className='header__div'>
                 <h1 className='header__div--title'>Host your website in <br /> less than 5 minutes</h1>
-                <p>with Hosterr, get your website up and running in no less then 5 minutes with the most competitive priccing packages available online</p>
+                <p>With Hosterr, get your website up and running in no less then 5 minutes with the most competitive priccing packages available online</p>
                 <div className='form'>
                     <input placeholder='Enter e-mail address' />
                     <button>Join Waitlist</button>
