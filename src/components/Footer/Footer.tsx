afd2a7c38dc30a7fc52d5908e3b953154c4a97d8
Satish9047 <satishprajapati930@gmail.com>
2024-02-19 12:44:14 +0545

    add: footer UI

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "./footer.scss";
 
 const Footer: React.FC = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <ul className='footer__social'>
+                <li><a href='#'>Facebook</a></li>
+                <li><a href='#'>Instagram</a></li>
+                <li><a href='#'>Twitter</a></li>
+            </ul>
+
+            <div className='footer__qa'>
+                <figure>
+                    <img src='/src/assets/Help-Avatar.svg' alt='avatar' />
+
+                </figure>
+                <p>Have any question?<br />
+                    <span>Talk to a specialist</span>
+                </p>
+            </div>
+        </div>
     )
 }
 
