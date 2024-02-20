2c2fac7232192a3d2d5939daf6911a9afbf3c71d
Satish9047 <satishprajapati930@gmail.com>
2024-02-20 12:32:16 +0545

    updated reposnsive fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     return (
         <div className='header'>
             <div className='header__div'>
-                <h1 className='header__div--title'>Host your website in <br /> less than 5 minutes</h1>
+                <h1 className='header__div--title'>Host your website in less than 5 minutes</h1>
                 <p>With Hosterr, get your website up and running in no less then 5 minutes with the most competitive priccing packages available online</p>
                 <div className='form'>
                     <input placeholder='Enter e-mail address' />
@@ -18,12 +18,16 @@ const Header: React.FC = () => {
                 </div>
             </div>
 
-            <figure className='hero'>
-                <img className='her0__img1' src='/src/assets/Hero Image (Model).png' alt='model' />
-                {/* <img className='her0__img2' src='/src/assets/Blue-Shape.svg' alt='model' />
-                <img className='her0__img3' src='/src/assets/Pink-Shape.svg' alt='model' />
-                <img className='her0__img4' src='/src/assets/Purple-Shape.svg' alt='model' /> */}
-            </figure>
+            <div className='image'>
+                <figure className='image__figure'>
+
+                    <img className='hero__img1' id='img1' src='/src/assets/Hero Image (Model).png' alt='model' />
+                    <img className='hero__img4' id='img2' src='/src/assets/Purple-Shape.svg' alt='model' />
+                    <img className='hero__img3' id='img3' src='/src/assets/Pink-Shape.svg' alt='model' />
+                    <img className='her0__img2' id='img4' src='/src/assets/Blue-Shape.svg' alt='model' />
+
+                </figure>
+            </div>
         </div>
     )
 }
