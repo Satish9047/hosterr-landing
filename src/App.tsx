220c7e767aa12fde30fa5065e8c666a008d978df
Satish9047 <satishprajapati930@gmail.com>
2024-02-21 09:19:51 +0545

    update the folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
+import React from "react";
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Header />
-      <Footer />
+    <div className="app" >
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
